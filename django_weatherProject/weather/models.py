dc12414f98c22de4898fc949ce98d0a93914c4a4
hay-0le <haley.olsen1@gmail.com>
2020-03-29 10:16:52 -0700

    Created models for starred cities (user favorites) and another for the weather data for each of those cities

---
+++ django_weatherProject/weather/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class StarredCity(models.Model):
+    city_id = models.CharField(max_length=150, primary_key=True)
+    city_name = models.CharField(max_length=100)
+    state = models.CharField(max_length=15)
+    is_home = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.city_name
+
+class CityWeatherData(models.Model):
+    city = models.ForeignKey(StarredCity, on_delete=models.CASCADE)
+    current_date = models.DateTimeField(auto_now=True)
+    current_temp = models.DecimalField(max_digits=50, decimal_places=2)
+    current_humidity = models.DecimalField(max_digits=50, decimal_places=2)
+    feels_like = models.DecimalField(max_digits=50, decimal_places=2)
+    todays_min_temp = models.DecimalField(max_digits=50, decimal_places=2)
+    todays_max_temp = models.DecimalField(max_digits=50, decimal_places=2)
+    todays_sunset = models.BigIntegerField()
+    todays_sunrise = models.BigIntegerField()
+
+    def __str__(self):
+        return self
