c3d39e8ccb2641ba7b5f98f3bbe7bc0d975e9449
hay-0le <haley.olsen1@gmail.com>
2020-03-30 08:42:17 -0700

    Form model, for user to add new starred city

---
+++ weather/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm, TextInput
+from .models import StarredCity
+
+class CityForm(ModelForm):
+    class Meta:
+        model = StarredCity 
+        fields = ['city_name']
+        widgets = {'city_name' : TextInput(attrs={'class' : 'input', 'placeholder' : 'City Name'})}
