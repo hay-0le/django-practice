0bf66d00562525eda036ee9ae3144be2b3c7fa65
hay-0le <haley.olsen1@gmail.com>
2020-03-30 08:05:11 -0700

    Registered models

--- weather/admin.py
+++ weather/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import StarredCity, CityWeatherData
 
-# Register your models here.
+admin.site.site_header = "Django Weather Admin"
+admin.site.site_title = "Django Weather Admin Area"
+admin.site.index_title = "Welcome to the Weather Admin Area"
+
+class CityWeatherInline(admin.TabularInline):
+    model = CityWeatherData
+    extra = 1
+
+class StarredCityAdmin(admin.ModelAdmin):
+    fieldsets = [('City', {'fields': ['city_name']}),]
+
+# admin.site.register(StarredCity)
+# admin.site.register(CityWeatherData)
+admin.site.register(StarredCity, StarredCityAdmin)
