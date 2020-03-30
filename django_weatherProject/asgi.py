ddfa884185533cb130c0f5ef31ea78b843886a02
hay-0le <haley.olsen1@gmail.com>
2020-03-30 08:59:52 -0700

    Automated files

--- django_weatherProject/asgi.py
+++ django_weatherProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_weather project.
+ASGI config for django_weatherProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_weather.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_weatherProject.settings')
 
 application = get_asgi_application()
--- django_weatherProject/urls.py
+++ django_weatherProject/urls.py
@@ -1,4 +1,4 @@
-"""django_weather URL Configuration
+"""django_weatherProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pages.urls')),
+    path('weather/', include('weather.urls')),
     path('admin/', admin.site.urls),
-    path('', include('weather.urls'))
 ]
