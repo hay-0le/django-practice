d492e85f1ccf010442dbd21a8d8b7d0c6e5f00a9
hay-0le <haley.olsen1@gmail.com>
2020-03-31 14:21:52 -0700

    Commented out code for unused model not being used yet

--- weather/admin.py
+++ weather/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
-from .models import StarredCity, CityWeatherData
+from .models import StarredCity
+# from .models import CityWeatherData
 
 admin.site.site_header = "Django Weather Admin"
 admin.site.site_title = "Django Weather Admin Area"
 admin.site.index_title = "Welcome to the Weather Admin Area"
 
-class CityWeatherInline(admin.TabularInline):
-    model = CityWeatherData
-    extra = 1
+# class CityWeatherInline(admin.TabularInline):
+#     model = CityWeatherData
+#     extra = 1
 
 class StarredCityAdmin(admin.ModelAdmin):
     fieldsets = [('City', {'fields': ['city_name']}),]
 
+admin.site.register(StarredCity, StarredCityAdmin)
 # admin.site.register(StarredCity)
 # admin.site.register(CityWeatherData)
-admin.site.register(StarredCity, StarredCityAdmin)
