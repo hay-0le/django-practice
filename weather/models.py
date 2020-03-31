fdabb9dbba1d39192eb591c1a7e8277f0ee7f27f
hay-0le <haley.olsen1@gmail.com>
2020-03-31 14:22:32 -0700

    Commented out model not currently being used

--- weather/models.py
+++ weather/models.py
@@ -4,12 +4,8 @@ from django.db import models
 
 class StarredCity(models.Model):
     # city_id = models.CharField(max_length=150, primary_key=True)
-    # city_id = models.CharField(max_length=150, primary_key=True)
     city_name = models.CharField(max_length=100)
-    # state = models.CharField(max_length=15, default=None)
     # is_home = models.BooleanField(default=False)
-    # temp = models.IntegerField(default=None)
-    # icon = models.CharField(max_length = 25, default=None)
 
     def __str__(self):
         return StarredCity.city_name
@@ -17,16 +13,16 @@ class StarredCity(models.Model):
     class Meta:
         verbose_name_plural = 'Starred Cities'
 
-class CityWeatherData(models.Model):
-    city = models.ForeignKey(StarredCity, on_delete=models.CASCADE)
-    current_date = models.DateTimeField(auto_now=True)
-    current_temp = models.DecimalField(max_digits=50, decimal_places=2)
-    current_humidity = models.DecimalField(max_digits=50, decimal_places=2)
-    feels_like = models.DecimalField(max_digits=50, decimal_places=2)
-    todays_min_temp = models.DecimalField(max_digits=50, decimal_places=2)
-    todays_max_temp = models.DecimalField(max_digits=50, decimal_places=2)
-    todays_sunset = models.BigIntegerField()
-    todays_sunrise = models.BigIntegerField()
+# class CityWeatherData(models.Model):
+#     city = models.ForeignKey(StarredCity, on_delete=models.CASCADE)
+#     current_date = models.DateTimeField(auto_now=True)
+#     current_temp = models.DecimalField(max_digits=50, decimal_places=2)
+#     current_humidity = models.DecimalField(max_digits=50, decimal_places=2)
+#     feels_like = models.DecimalField(max_digits=50, decimal_places=2)
+#     todays_min_temp = models.DecimalField(max_digits=50, decimal_places=2)
+#     todays_max_temp = models.DecimalField(max_digits=50, decimal_places=2)
+#     todays_sunset = models.BigIntegerField()
+#     todays_sunrise = models.BigIntegerField()
 
-    def __str__(self):
-        return self
+#     def __str__(self):
+#         return self
