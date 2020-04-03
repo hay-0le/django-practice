4d0c3304470312461124d36af0833c1d1541a3f5
hay-0le <haley.olsen1@gmail.com>
2020-04-03 07:53:15 -0700

    Added city_id and is_home to model

--- weather/models.py
+++ weather/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class StarredCity(models.Model):
-    # city_id = models.CharField(max_length=150, primary_key=True)
+    city_id = models.CharField(max_length=150, primary_key=True)
     city_name = models.CharField(max_length=100)
-    # is_home = models.BooleanField(default=False)
+    is_home = models.BooleanField(default=False)
 
     def __str__(self):
         return StarredCity.city_name
